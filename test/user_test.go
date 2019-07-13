f9f4b8bcf2431099dd8fee813954fa391acdc67b
youngxhui <youngxhui@gmail.com>
2019-07-13 08:30:28 +0800

    修改命名规范问题

--- test/user_test.go
+++ test/user_test.go
@@ -13,8 +13,8 @@ func TestUserSave(t *testing.T) {
 	username := "lisi"
 	router := initRouter.SetupRouter()
 	w := httptest.NewRecorder()
-	getReq, _ := http.NewRequest(http.MethodGet, "/user/"+username, nil)
-	router.ServeHTTP(w, getReq)
+	req, _ := http.NewRequest(http.MethodGet, "/user/"+username, nil)
+	router.ServeHTTP(w, req)
 	assert.Equal(t, http.StatusOK, w.Code)
 	assert.Equal(t, "用户"+username+"已经保存", w.Body.String())
 }
@@ -24,8 +24,8 @@ func TestUserSaveQuery(t *testing.T) {
 	age := 18
 	router := initRouter.SetupRouter()
 	w := httptest.NewRecorder()
-	getReq, _ := http.NewRequest(http.MethodGet, "/user?name="+username+"&age="+strconv.Itoa(age), nil)
-	router.ServeHTTP(w, getReq)
+	req, _ := http.NewRequest(http.MethodGet, "/user?name="+username+"&age="+strconv.Itoa(age), nil)
+	router.ServeHTTP(w, req)
 	assert.Equal(t, http.StatusOK, w.Code)
 	assert.Equal(t, "用户:"+username+",年龄:"+strconv.Itoa(age)+"已经保存", w.Body.String())
 }
