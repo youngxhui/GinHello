52abc3b310125ecb58c63b2f3a7a0824cf405096
youngxhui <youngxhui@gmail.com>
2019-07-13 16:01:16 +0800

    Gin 模板使用代码示例

--- initRouter/initRouter.go
+++ initRouter/initRouter.go
@@ -12,20 +12,6 @@ func SetupRouter() *gin.Engine {
 
 	index := router.Group("/")
 	{
-		//// 添加 Get 请求路由
-		//index.GET("/", retHelloGinAndMethod)
-		//// 添加 Post 请求路由
-		//index.POST("/", retHelloGinAndMethod)
-		//// 添加 Put 请求路由
-		//index.PUT("/", retHelloGinAndMethod)
-		//// 添加 Delete 请求路由
-		//index.DELETE("/", retHelloGinAndMethod)
-		//// 添加 Patch 请求路由
-		//index.PATCH("/", retHelloGinAndMethod)
-		//// 添加 Head 请求路由
-		//index.HEAD("/", retHelloGinAndMethod)
-		//// 添加 Options 请求路由
-		//index.OPTIONS("/", retHelloGinAndMethod)
 		index.Any("", retHelloGinAndMethod)
 	}
 	// 添加 user
@@ -34,9 +20,6 @@ func SetupRouter() *gin.Engine {
 		userRouter.GET("/:name", user.Save)
 		userRouter.GET("", user.SaveByQuery)
 	}
-
-	//router.GET("/user/:name", user.Save)
-	//router.GET("/user", user.SaveByQuery)
 	return router
 }
 
