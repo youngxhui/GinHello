afebcb7ef4c0d86500d3302b820c338faad331cf
youngxhui <youngxhui@gmail.com>
2019-07-13 16:03:27 +0800

    更正错误代码

--- initRouter/initRouter.go
+++ initRouter/initRouter.go
@@ -12,21 +12,21 @@ func SetupRouter() *gin.Engine {
 
 	index := router.Group("/")
 	{
-		//// 添加 Get 请求路由
-		//index.GET("/", retHelloGinAndMethod)
-		//// 添加 Post 请求路由
-		//index.POST("/", retHelloGinAndMethod)
-		//// 添加 Put 请求路由
-		//index.PUT("/", retHelloGinAndMethod)
-		//// 添加 Delete 请求路由
-		//index.DELETE("/", retHelloGinAndMethod)
-		//// 添加 Patch 请求路由
-		//index.PATCH("/", retHelloGinAndMethod)
-		//// 添加 Head 请求路由
-		//index.HEAD("/", retHelloGinAndMethod)
-		//// 添加 Options 请求路由
-		//index.OPTIONS("/", retHelloGinAndMethod)
-		index.Any("", retHelloGinAndMethod)
+		// 添加 Get 请求路由
+		index.GET("", retHelloGinAndMethod)
+		// 添加 Post 请求路由
+		index.POST("", retHelloGinAndMethod)
+		// 添加 Put 请求路由
+		index.PUT("", retHelloGinAndMethod)
+		// 添加 Delete 请求路由
+		index.DELETE("", retHelloGinAndMethod)
+		// 添加 Patch 请求路由
+		index.PATCH("", retHelloGinAndMethod)
+		// 添加 Head 请求路由
+		index.HEAD("", retHelloGinAndMethod)
+		// 添加 Options 请求路由
+		index.OPTIONS("", retHelloGinAndMethod)
+		//index.Any("", retHelloGinAndMethod)
 	}
 	// 添加 user
 	userRouter := router.Group("/user")
