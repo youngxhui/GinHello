3b4f7ce24eb2c9e465e6bdc9f30891944d03f8d6
youngxhui <youngxhui@gmail.com>
2021-05-20 20:48:21 +0800

    完成重构

--- router/router_test.go
+++ router/router_test.go
@@ -1,10 +1,12 @@
 package router
 
 import (
-	"github.com/stretchr/testify/assert"
 	"net/http"
 	"net/http/httptest"
+	"strconv"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 // router("/") get 测试
@@ -76,4 +78,35 @@ func TestIndexOptionsRouter(t *testing.T) {
 	router.ServeHTTP(w, req)
 	assert.Equal(t, http.StatusOK, w.Code)
 	assert.Equal(t, "hello gin options method", w.Body.String())
+}
+
+func TestUserSave(t *testing.T) {
+	username := "lisi"
+	router := SetupRouter()
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest(http.MethodGet, "/user/"+username, nil)
+	router.ServeHTTP(w, req)
+	assert.Equal(t, http.StatusOK, w.Code)
+	assert.Equal(t, "用户"+username+"已经保存", w.Body.String())
+}
+
+func TestUserSaveQuery(t *testing.T) {
+	username := "lisi"
+	age := 18
+	router := SetupRouter()
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest(http.MethodGet, "/user?name="+username+"&age="+strconv.Itoa(age), nil)
+	router.ServeHTTP(w, req)
+	assert.Equal(t, http.StatusOK, w.Code)
+	assert.Equal(t, "用户:"+username+",年龄:"+strconv.Itoa(age)+"已经保存", w.Body.String())
+}
+
+func TestUserSaveWithNotAge(t *testing.T) {
+	username := "lisi"
+	router := SetupRouter()
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest(http.MethodGet, "/user?name="+username, nil)
+	router.ServeHTTP(w, req)
+	assert.Equal(t, http.StatusOK, w.Code)
+	assert.Equal(t, "用户:"+username+",年龄:20已经保存", w.Body.String())
 }
